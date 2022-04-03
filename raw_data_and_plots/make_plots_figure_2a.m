19cfcaa366d027d74d6da563f88937986d3de222
David Bernstein <dbernstein@berkeley.edu>
2022-04-03 15:23:02 -0700

    Updated AA Diffusion and Re-Run
    Updated the Amino Acid diffusion constant and re-ran the analysis. Saved a new matlab script to run the analysis and new results ending with '_2'

--- raw_data_and_plots/make_plots_figure_2a.m
+++ raw_data_and_plots/make_plots_figure_2a.m
@@ -222,3 +222,186 @@ for J = 1:5
         axis([0 xmax ymin ymax])
     end
 end
+
+%% Lysine and Isoleucine Data
+
+% Load Data
+% Lysine
+sheet = 1;
+file = '/Users/david/Documents/GitHub/co_culture_device/raw_data_and_plots/Figure 2 -- AssayAnalysis-BioMeDiffusion.xlsx';
+% Standard curve
+range = 'C29:H30';
+[L_s,~,~] = xlsread(file,sheet,range);
+% Standard curve control
+range = 'C35:H36';
+[L_s_c,~,~] = xlsread(file,sheet,range);
+% Data
+range = 'C46:N48';
+[L_d,~,~] = xlsread(file,sheet,range);
+% Data control
+range = 'C53:N55';
+[L_d_c,~,~] = xlsread(file,sheet,range);
+
+% Isoleucine
+sheet = 2;
+% Standard curve
+range = 'C31:J33';
+[I_s,~,~] = xlsread(file,sheet,range);
+% Standard curve control
+range = 'C38:J39';
+[I_s_c,~,~] = xlsread(file,sheet,range);
+% Data
+range = 'C49:N51';
+[I_d,~,~] = xlsread(file,sheet,range);
+% Data control
+range = 'C56:N58';
+[I_d_c,~,~] = xlsread(file,sheet,range);
+
+
+% Plot Standard Curve
+figure(7)
+% Lysine
+L_st = [100,50,25,12.5,6.25,0]; %[micro molar]
+subplot(1,2,1)
+hold on
+% Average standard
+%a_L_s_c = mean(L_s_c,'all'); %blanks
+a_L_s_c = mean([L_s(1,6),L_s(2,6)]); %0 conc samples
+% Keep track of data for linear regression
+DATA_L = [];
+for I = 1:6 %concentrations
+    for J = 1:2 %replicates
+        plot(L_s(J,I)-a_L_s_c,L_st(I),'k.','markersize',10)
+        DATA_L = [DATA_L;[L_st(I),L_s(J,I)-a_L_s_c]];
+    end
+end
+% Linear regression model
+T_L = array2table(DATA_L);
+T_L.Properties.VariableNames(1:2) = {'C','A'};
+mdl_L = fitlm(T_L,'C~A');
+subplot(1,2,1)
+X = [min(DATA_L(:,2)),max(DATA_L(:,2))];
+Y = mdl_L.Coefficients{1,1} + mdl_L.Coefficients{2,1}.*X;
+plot(X,Y,'color',red,'linewidth',lw)
+text(0.1,20,strcat('R-squared:',num2str(round(mdl_L.Rsquared.Ordinary,3))),'fontsize',14)
+% Format Plot
+ylim([-1 120])
+title('Lysine')
+xlabel('OD 550')
+ylabel('Conc. uM')
+set(gca,'fontsize',16)
+set(gcf,'renderer','painters')
+
+
+% Isoleucine
+I_st = [1000,500,250,125,62.5,31.25,15.625,0]; %[micro molar]
+subplot(1,2,2)
+hold on
+% Average standard
+%a_I_s_c = mean(I_s_c,'all'); %blanks
+a_I_s_c = mean([I_s(1,8),I_s(2,8),I_s(3,8)]); %0 conc samples
+% Keep track of data for linear regression
+DATA_I = [];
+for I = 1:8 %concentrations
+    for J = 1:2 %replicates
+        plot(I_s(J,I)-a_I_s_c,I_st(I),'k.','markersize',10)
+        DATA_I = [DATA_I;[I_st(I),I_s(J,I)-a_I_s_c]];
+    end
+end
+% Linear regression model
+T_I = array2table(DATA_I);
+T_I.Properties.VariableNames(1:2) = {'C','A'};
+mdl_I = fitlm(T_I,'C~A');
+subplot(1,2,2)
+X = [min(DATA_I(:,2)),max(DATA_I(:,2))];
+Y = mdl_I.Coefficients{1,1} + mdl_I.Coefficients{2,1}.*X;
+plot(X,Y,'color',red,'linewidth',lw)
+text(0.5,100,strcat('R-squared:',num2str(round(mdl_I.Rsquared.Ordinary,3))),'fontsize',14)
+% Format Plot
+ylim([-20 1000])
+title('Isoleucine')
+xlabel('OD 450')
+ylabel('Conc. uM')
+set(gca,'fontsize',16)
+set(gcf,'renderer','painters')
+
+% Plot Amino Acid Concentration Curves
+figure(8)
+time = [0,1.5,3,6,12,24]; %hours
+% Lysine
+% Convert data to micro molar
+% Normalize with blank
+a_L_c = a_L_s_c; %Use blank from standard curve
+%a_L_C = mean(L_d_c,'all'); %Use blank from data
+L_d_n = L_d - a_L_c;
+% Convert to micro molar with linear regression
+L_d_um = (L_d_n.*mdl_L.Coefficients{2,1}+mdl_L.Coefficients{1,1})*4;
+% Plot data
+subplot(1,2,1)
+hold on
+for I = 1:6 %time points
+    l_ind = I*2-1;
+    r_ind = I*2;
+    for J = 1:3 %replicates
+        % left well
+        plot(time(I),L_d_um(J,l_ind),'b.','markersize',10)
+        % right well
+        plot(time(I),L_d_um(J,r_ind),'r.','markersize',10)
+        % total
+        plot(time(I),L_d_um(J,l_ind)+L_d_um(J,r_ind),'k.','markersize',10)
+    end
+end
+% Plot means
+m_L_d_um = mean(L_d_um,1);
+m_L_d_um_left = m_L_d_um([1,3,5,7,9,11]);
+m_L_d_um_right = m_L_d_um([2,4,6,8,10,12]);
+m_L_d_um_total = m_L_d_um_left+m_L_d_um_right;
+plot(time,m_L_d_um_left,'b:','linewidth',2)
+plot(time,m_L_d_um_right,'r:','linewidth',2)
+plot(time,m_L_d_um_total,'k:','linewidth',2)
+% Format Plot
+title('Lysine')
+xlabel('Time [hours]')
+ylabel('Conc. [uM]')
+set(gca,'fontsize',16)
+set(gcf,'renderer','painters')
+
+% Isoleucine
+% Convert data to micro molar
+% Normalize with blank
+a_I_c = a_I_s_c; %Use blank from standard curve
+%a_L_C = mean(L_d_c,'all'); %Use blank from data
+I_d_n = I_d - a_I_c;
+% Convert to micro molar with linear regression
+I_d_um = I_d_n.*mdl_I.Coefficients{2,1}+mdl_I.Coefficients{1,1};
+% Plot data
+subplot(1,2,2)
+hold on
+for I = 1:6 %time points
+    l_ind = I*2-1;
+    r_ind = I*2;
+    for J = 1:3 %replicates
+        % left well
+        plot(time(I),I_d_um(J,l_ind),'b.','markersize',10)
+        % right well
+        plot(time(I),I_d_um(J,r_ind),'r.','markersize',10)
+        % total
+        plot(time(I),I_d_um(J,l_ind)+I_d_um(J,r_ind),'k.','markersize',10)
+    end
+end
+% Plot means
+m_I_d_um = mean(I_d_um,1);
+m_I_d_um_left = m_I_d_um([1,3,5,7,9,11]);
+m_I_d_um_right = m_I_d_um([2,4,6,8,10,12]);
+m_I_d_um_total = m_I_d_um_left+m_I_d_um_right;
+plot(time,m_I_d_um_left,'b:','linewidth',2)
+plot(time,m_I_d_um_right,'r:','linewidth',2)
+plot(time,m_I_d_um_total,'k:','linewidth',2)
+% Format Plot
+title('Isoleucine')
+xlabel('Time [hours]')
+ylabel('Conc. [uM]')
+set(gca,'fontsize',16)
+set(gcf,'renderer','painters')
+
+
--- raw_data_and_plots/make_plots_figure_2b.m
+++ raw_data_and_plots/make_plots_figure_2b.m
@@ -200,4 +200,191 @@ set(gca,'fontsize',14)
 xticks([0.03,0.1,0.2,0.4])
 xticklabels({'0.03','0.1','0.2','0.4'})
 
+%% Lysine and Isoleucine
+% Load data and calculate concentrations
 
+% Load Data
+% Lysine
+sheet = 1;
+file = '/Users/david/Documents/GitHub/co_culture_device/raw_data_and_plots/Figure 2 -- AssayAnalysis-BioMeDiffusion.xlsx';
+% Standard curve
+range = 'C29:H30';
+[L_s,~,~] = xlsread(file,sheet,range);
+% Standard curve control
+range = 'C35:H36';
+[L_s_c,~,~] = xlsread(file,sheet,range);
+% Data
+range = 'C46:N48';
+[L_d,~,~] = xlsread(file,sheet,range);
+% Data control
+range = 'C53:N55';
+[L_d_c,~,~] = xlsread(file,sheet,range);
+
+% Isoleucine
+sheet = 2;
+% Standard curve
+range = 'C31:J33';
+[I_s,~,~] = xlsread(file,sheet,range);
+% Standard curve control
+range = 'C38:J39';
+[I_s_c,~,~] = xlsread(file,sheet,range);
+% Data
+range = 'C49:N51';
+[I_d,~,~] = xlsread(file,sheet,range);
+% Data control
+range = 'C56:N58';
+[I_d_c,~,~] = xlsread(file,sheet,range);
+
+
+% Plot Standard Curve
+% Lysine
+L_st = [100,50,25,12.5,6.25,0]; %[micro molar]
+% Average standard
+%a_L_s_c = mean(L_s_c,'all'); %blanks
+a_L_s_c = mean([L_s(1,6),L_s(2,6)]); %0 conc samples
+% Keep track of data for linear regression
+DATA_L = [];
+for I = 1:6 %concentrations
+    for J = 1:2 %replicates
+        DATA_L = [DATA_L;[L_st(I),L_s(J,I)-a_L_s_c]];
+    end
+end
+% Linear regression model
+T_L = array2table(DATA_L);
+T_L.Properties.VariableNames(1:2) = {'C','A'};
+mdl_L = fitlm(T_L,'C~A');
+X = [min(DATA_L(:,2)),max(DATA_L(:,2))];
+Y = mdl_L.Coefficients{1,1} + mdl_L.Coefficients{2,1}.*X;
+
+% Isoleucine
+I_st = [1000,500,250,125,62.5,31.25,15.625,0]; %[micro molar]
+% Average standard
+%a_I_s_c = mean(I_s_c,'all'); %blanks
+a_I_s_c = mean([I_s(1,8),I_s(2,8),I_s(3,8)]); %0 conc samples
+% Keep track of data for linear regression
+DATA_I = [];
+for I = 1:8 %concentrations
+    for J = 1:2 %replicates
+        DATA_I = [DATA_I;[I_st(I),I_s(J,I)-a_I_s_c]];
+    end
+end
+% Linear regression model
+T_I = array2table(DATA_I);
+T_I.Properties.VariableNames(1:2) = {'C','A'};
+mdl_I = fitlm(T_I,'C~A');
+X = [min(DATA_I(:,2)),max(DATA_I(:,2))];
+Y = mdl_I.Coefficients{1,1} + mdl_I.Coefficients{2,1}.*X;
+
+% Plot Amino Acid Concentration Curves
+time = [0,1.5,3,6,12,24]; %hours
+% Lysine
+% Convert data to micro molar
+% Normalize with blank
+a_L_c = a_L_s_c; %Use blank from standard curve
+%a_L_c = mean(L_d_c,'all'); %Use blank from data
+L_d_n = L_d - a_L_c;
+% Convert to micro molar with linear regression
+L_d_um = (L_d_n.*mdl_L.Coefficients{2,1}+mdl_L.Coefficients{1,1})*4;
+
+% Isoleucine
+% Convert data to micro molar
+% Normalize with blank
+a_I_c = a_I_s_c; %Use blank from standard curve
+%a_I_c = mean(I_d_c,'all'); %Use blank from data
+I_d_n = I_d - a_I_c;
+% Convert to micro molar with linear regression
+I_d_um = I_d_n.*mdl_I.Coefficients{2,1}+mdl_I.Coefficients{1,1};
+
+
+% Fit to exponential
+% uM to mmol
+vol = 250e-6; %[L]
+m_L = (L_d_um/1000)*vol;
+m_I = (I_d_um/1000)*vol;
+K_L = (mean([L_d_um(1,1),L_d_um(2,1),L_d_um(3,1)])/1000)*vol;
+K_I = (mean([I_d_um(1,1),I_d_um(2,1),I_d_um(3,1)])/1000)*vol;
+
+% Transform
+% mmol-K/2
+T_m_L = m_L-(K_L/2);
+T_m_I = m_I-(K_I/2);
+
+% Plot transformed data
+figure(3)
+% Lysine
+subplot(1,2,1)
+hold on
+for I = 1:6 %time points
+    l_ind = I*2-1;
+    r_ind = I*2;
+    for J = 1:3 %replicates
+        % left well
+        plot(time(I),T_m_L(J,l_ind),'b.','markersize',10)
+        % right well
+        plot(time(I),T_m_L(J,r_ind),'r.','markersize',10)
+        % total
+        plot(time(I),T_m_L(J,l_ind)+T_m_L(J,r_ind),'k.','markersize',10)
+    end
+end
+subplot(1,2,2)
+hold on
+for I = 1:6 %time points
+    l_ind = I*2-1;
+    r_ind = I*2;
+    for J = 1:3 %replicates
+        % left well
+        plot(time(I),T_m_I(J,l_ind),'b.','markersize',10)
+        % right well
+        plot(time(I),T_m_I(J,r_ind),'r.','markersize',10)
+        % total
+        plot(time(I),T_m_I(J,l_ind)+T_m_I(J,r_ind),'k.','markersize',10)
+    end
+end
+
+% Fit exponential
+time = [0,1.5,3,6,12,24];
+x = [time,time,time];
+X = linspace(min(time),max(time),100);
+
+% Lysine
+% Left
+y = [T_m_L(1,[1,3,5,7,9,11]),T_m_L(2,[1,3,5,7,9,11]),T_m_L(3,[1,3,5,7,9,11])];
+f = fit(x',y','exp1','lower',[K_L/2,-Inf],'upper',[K_L/2,Inf],'TolFun',1e-11);
+D_L_l = (f.b*vol)/-2;
+% plot exponential fit
+Y = f.a*exp(f.b*X);
+subplot(1,2,1)
+plot(X,Y,'b:','linewidth',2)
+% Right
+y = [T_m_L(1,[2,4,6,8,10,12]),T_m_L(2,[2,4,6,8,10,12]),T_m_L(3,[2,4,6,8,10,12])];
+f = fit(x',y','exp1','lower',[-1*K_L/2,-Inf],'upper',[-1*K_L/2,Inf],'TolFun',1e-11);
+D_L_r = (f.b*vol)/-2;
+% plot exponential fit
+Y = f.a*exp(f.b*X);
+subplot(1,2,1)
+plot(X,Y,'r:','linewidth',2)
+
+% Isoleucine
+% Left
+y = [T_m_I(1,[1,3,5,7,9,11]),T_m_I(2,[1,3,5,7,9,11]),T_m_I(3,[1,3,5,7,9,11])];
+f = fit(x',y','exp1','lower',[K_I/2,-Inf],'upper',[K_I/2,Inf],'TolFun',1e-11);
+D_I_l = (f.b*vol)/-2;
+% plot exponential fit
+Y = f.a*exp(f.b*X);
+subplot(1,2,2)
+plot(X,Y,'b:','linewidth',2)
+% Right
+y = [T_m_I(1,[2,4,6,8,10,12]),T_m_I(2,[2,4,6,8,10,12]),T_m_I(3,[2,4,6,8,10,12])];
+f = fit(x',y','exp1','lower',[-1*K_I/2,-Inf],'upper',[-1*K_I/2,Inf],'TolFun',1e-11);
+D_I_r = (f.b*vol)/-2;
+% plot exponential fit
+Y = f.a*exp(f.b*X);
+subplot(1,2,2)
+plot(X,Y,'r:','linewidth',2)
+
+% Plote diffusion coefficients on figure
+figure(2)
+plot(0.1,D_L_l,'b.','markersize',15)
+plot(0.1,D_L_r,'b>','markersize',7)
+plot(0.1,D_I_l,'g.','markersize',15)
+plot(0.1,D_I_r,'g>','markersize',7)
